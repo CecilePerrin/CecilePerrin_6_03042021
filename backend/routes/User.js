391c5dbd89e73766f764cc5060e056ab444b82e8
Cécile Perrin <cperrin.387@gmail.com>
2021-04-24 13:20:07 +0200

    add axpress rate limit

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const router = express.Router();
+const rateLimit = require('../middleware/limit');
 
 const userCrl = require('../controllers/User');
 
 
 router.post('/signup', userCrl.signup);
-router.post('/login', userCrl.login);
+router.post('/login', rateLimit.limiter, userCrl.login);
 
 module.exports = router;
